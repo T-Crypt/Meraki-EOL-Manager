fb939227b35c107f8b6b5b4b12bf061d92241dd9
Tyler <owentl@owentl.com>
2025-01-24 07:14:28 -0500

    remove meraki logs, move to python logging and add ability to choose all orgs

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-api_key = 'MERAKI_API_KEY_HERE'
+api_key = "MERAKI_API_KEY_HERE"
+html_output = False  # Set to True to output to HTML and PDF
+output_log = False  # Set to True to output to log file
+print_console = False  # Set to True to enable console output
